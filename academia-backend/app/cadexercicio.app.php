8193ce6223ede4411a2ae88302095eab732d4876
bryane <bryan.barreto@icloud.com>
2020-04-22 12:41:53 -0300

    arrumando bugs

--- academia-backend/app/cadexercicio.app.php
+++ academia-backend/app/cadexercicio.app.php
@@ -23,8 +23,8 @@
 
         case 'incluir': 
             try{
-                echo "chegou aqui";
                 $objeto->setDescricao($_REQUEST['descricao']);
+                echo "chegou aqui";
                 $objeto->incluir();
                 $json = true;
             }catch(Exception $e){
